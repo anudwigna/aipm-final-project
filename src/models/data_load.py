5459d7577dadfb7eb8192949130a0e688e30bbe4
Kassem Bou Zeid <ksmbzd@Gmail.com>
2023-04-10 14:56:26 +0200

    Data Processing Updated

--- src/models/data_load.py
+++ src/models/data_load.py
@@ -5,17 +5,6 @@ def load_data():
     project_path = os.path.dirname(os.path.abspath("requirements.txt"))
     #Import the training and test data from CSV files into pandas dataframes
     train_df = pd.read_csv(f"{project_path}/src/data/train.csv")
-    test_df = pd.read_csv(f"{project_path}/src/data/test.csv")
-    return train_df, test_df
+    return train_df
 
-train_df, test_df = load_data()
-
-# #Print the shape of the training and test dataframes to verify the number of rows and columns
-# print(f"Training data shape: {train_df.shape}")
-# print(f"Test data shape: {test_df.shape}")
-
-# #View the first few rows of the training data to check the data has been loaded properly
-# print(train_df.head(5))
-
-# #View the first few rows of the test data to check the data has been loaded properly
-# print(test_df.head(5))
+train_df= load_data()
