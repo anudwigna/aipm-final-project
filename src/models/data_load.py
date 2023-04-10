613830ab7f2d4498c1a8727cb0c03a970c6998cb
Kassem Bou Zeid <ksmbzd@Gmail.com>
2023-04-10 11:45:05 +0200

    Load and Explore Data for Model

--- src/models/data_load.py
+++ src/models/data_load.py
@@ -3,12 +3,19 @@ import os
 
 def load_data():
     project_path = os.path.dirname(os.path.abspath("requirements.txt"))
+    #Import the training and test data from CSV files into pandas dataframes
     train_df = pd.read_csv(f"{project_path}/src/data/train.csv")
     test_df = pd.read_csv(f"{project_path}/src/data/test.csv")
     return train_df, test_df
 
 train_df, test_df = load_data()
 
-print(train_df.head())
+#Print the shape of the training and test dataframes to verify the number of rows and columns
+print(f"Training data shape: {train_df.shape}")
+print(f"Test data shape: {test_df.shape}")
 
-# load_data()
+#View the first few rows of the training data to check the data has been loaded properly
+print(train_df.head(5))
+
+#View the first few rows of the test data to check the data has been loaded properly
+print(test_df.head(5))
