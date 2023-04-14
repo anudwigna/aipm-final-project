86dd260a43abafb5b38f6d9084dfd0c1dd27be38
Kassem Bou Zeid <ksmbzd@Gmail.com>
2023-04-14 09:28:11 +0200

    Data_load(Updated to last version)

--- src/models/data_load.py
+++ src/models/data_load.py
@@ -1,10 +1,11 @@
-import pandas as pd
 import os
+import pandas as pd
+
 
 def load_data():
     project_path = os.path.dirname(os.path.abspath("requirements.txt"))
-    #Import the training and test data from CSV files into pandas dataframes
     train_df = pd.read_csv(f"{project_path}/src/data/train.csv")
-    return train_df
+    test_df = pd.read_csv(f"{project_path}/src/data/test.csv")
+    return train_df, test_df
 
-train_df= load_data()
+train_df, test_df = load_data()
--- src/models/data_processing.py
+++ src/models/data_processing.py
@@ -1,17 +1,12 @@
-from data_load import train_df
+from data_load import train_df,test_df
 
-def preprocess_train_data(train_df):
-    '''
-    Preprocess the training data by dropping unnecessary features and removing outliers.
-    '''
-    # Save the 'Id' column for both train and test dataframes
+def preprocess_train_data(train_df,test_df):
     train_ID = train_df['Id']
-
-    # Drop the 'Id' column from both train and test dataframes since it's not necessary for the prediction process.
+    test_ID = test_df['Id']
     train_df.drop("Id", axis=1, inplace=True)
-
-    # Drop the rows with GrLivArea greater than 4000 and SalePrice less than 300000
+    test_df.drop("Id", axis=1, inplace=True)
     train_df = train_df.drop(train_df[(train_df['GrLivArea'] > 4000) & (train_df['SalePrice'] < 300000)].index)
+    return train_df, train_ID,test_df, test_ID
 
-    return train_df
-
+train_df, train_ID,test_df, test_ID =preprocess_train_data(train_df,test_df)
+  
